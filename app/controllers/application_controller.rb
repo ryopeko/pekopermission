21326316d2e910f3ab951032dc5d255236f1ef61
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-11-28 17:48:55 +0900

    Add auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def authenticate
+    redirect_to signin_path, notice: 'Please, Sign in' unless signed_in?
+  end
+
+  def sign_in(user)
+    remember_token = SecureRandom.urlsafe_base64
+    session[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = session[:remember_token] || return
+    @current_user ||= User.find_by(remember_token: User.encrypt(remember_token))
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates_uniqueness_of :email
+  validates_presence_of :email, :password, on: :create
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token)
+  end
 end
