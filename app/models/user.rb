9f57f5f22faf2c9ecfe597719fe74ee9594ed711
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-12-07 20:20:30 +0900

    Fix columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_uniqueness_of :email
-  validates_presence_of :email, :password, on: :create
+  validates_uniqueness_of :name
+  validates_presence_of :name, :password, on: :create
 
   def self.encrypt(token)
     Digest::SHA1.hexdigest(token)
