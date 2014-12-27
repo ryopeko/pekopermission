446ee2901c8289176ff8a562700332f4952f246c
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-12-27 21:21:24 +0900

    Create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_filter :authenticate, only: [:new, :create]
+
   def dashboard
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :permissions
+
+  resources :users, only: [:new, :create]
+
   get 'dashboard' => 'users#dashboard'
 
   match '/login', to: 'sessions#new', via: :get
