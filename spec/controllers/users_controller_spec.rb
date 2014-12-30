0c8b230a81c3e2d90c8e761ec876be6f5cce57f2
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-12-30 21:52:53 +0900

    Add accessibility spec of users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+  before do
+    @user = create(:user)
+  end
+
+  describe 'Accessibility' do
+    context 'When signed out' do
+      before do
+        test_signout
+      end
+
+      context 'GET new' do
+        before { get :new }
+
+        it { expect(response).to be_success }
+      end
+
+      context 'POST create' do
+        before { post :create, user: {name: 'foo', password: 'bar'} }
+
+        it { expect(response).to redirect_to dashboard_path }
+      end
+    end
+
+    context 'When signed in' do
+      before do
+        test_signin(@user)
+      end
+
+      context 'GET new' do
+        before { get :new }
+
+        it { expect(response).to redirect_to dashboard_path }
+      end
+
+      context 'POST create' do
+        before { post :create, user: {name: 'foo', password: 'bar'} }
+
+        it { expect(response).to redirect_to dashboard_path }
+      end
+    end
+  end
 end
