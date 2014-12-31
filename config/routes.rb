eacd00f8589f685dc3ad3049ac5ff4c84e49c0e1
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-12-31 19:22:42 +0900

    Fix path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   get 'dashboard' => 'users#dashboard'
 
-  match '/login', to: 'sessions#new', via: :get
-  match '/logout', to: 'sessions#destroy', via: :delete
+  match '/signin', to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
