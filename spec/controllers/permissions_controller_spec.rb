91887a9878bfb3c52872d55c0d4c91c98d0cf289
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-01 20:03:01 +0900

    accessibility specs

--- spec/controllers/permissions_controller_spec.rb
+++ spec/controllers/permissions_controller_spec.rb
@@ -1,4 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe PermissionsController, :type => :controller do
+  before do
+    @user = create(:user)
+  end
+
+  describe 'Accessibility' do
+    context 'When signed out' do
+      before do
+        test_signout
+      end
+
+      context 'GET new' do
+        before { get :new }
+
+        it { expect(response).to redirect_to signin_path }
+      end
+
+      context 'POST create' do
+        before { post :create, user: {name: 'foo', password: 'bar'} }
+
+        it { expect(response).to redirect_to signin_path }
+      end
+
+      context 'GET show' do
+        before { get :show, id: 1 }
+
+        it { expect(response).to redirect_to signin_path }
+      end
+
+      context 'GET index' do
+        before { get :index }
+
+        it { expect(response).to redirect_to signin_path }
+      end
+    end
+
+    context 'When signed in' do
+      before do
+        test_signin(@user)
+      end
+
+      context 'GET new' do
+        before { get :new }
+
+        it { expect(response).to be_success }
+      end
+
+      context 'POST create' do
+        before { post :create, permission: {name: 'foo', enabled: true} }
+
+        it { expect(response).to be_success }
+      end
+    end
+  end
+
 end
