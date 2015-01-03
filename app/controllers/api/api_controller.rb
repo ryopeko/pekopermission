4868b6d1d9fa64b70f453b3e531c4de552cae34a
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-03 20:40:39 +0900

    Add api base controller

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,7 @@
+class APIController < ApplicationController
+  include Garage::ControllerHelper
+
+  def current_resource_owner
+    @current_resource_owner ||= User.find(resource_owner_id) if resource_owner_id
+  end
+end
