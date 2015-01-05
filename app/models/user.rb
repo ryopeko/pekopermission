5887889034fd23ac61ba8db5e975d87f02b38d69
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-05 19:39:32 +0900

    Add accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  include Garage::Representer
+
   has_secure_password
 
   has_many :permissions
+
+  property :id
+  property :name
 
   validates_uniqueness_of :name
   validates_presence_of :name, :password, on: :create
