68ab7b72e248852ea02fadc18959b4eba3c09501
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-08 15:08:46 +0900

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   match '/signin', to: 'sessions#new', via: :get
   match '/signout', to: 'sessions#destroy', via: :delete
+
+  scope path: :api, module: :api do
+    resources :users, only: [:show, :index]
+  end
 end
