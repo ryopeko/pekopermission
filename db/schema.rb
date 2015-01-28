3e99c532c8a15e3db2e6cac0683336f86317dbe1
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-28 21:23:11 +0900

    Add Role model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105124512) do
+ActiveRecord::Schema.define(version: 20150128121335) do
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
@@ -58,6 +58,12 @@ ActiveRecord::Schema.define(version: 20150105124512) do
     t.boolean  "enabled",                default: true
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
