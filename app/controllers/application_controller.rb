a2473f5769ea8a5ab851c1db64975a2380ea6a02
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-02-05 20:35:00 +0900

    Move to sessions_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
   before_action :authenticate
-
-  def authenticate
-    redirect_to signin_path, notice: 'Please, Sign in' unless signed_in?
-  end
-
-  def signin(user)
-    remember_token = SecureRandom.urlsafe_base64
-    session[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    @current_user = user
-  end
-
-  def signout
-    current_user.try(:update_attribute, :remember_token, nil)
-    reset_session
-
-    @current_user = nil
-  end
-
-  def current_user
-    remember_token = session[:remember_token] || return
-    @current_user ||= User.find_by(remember_token: User.encrypt(remember_token))
-  end
-
-  def signed_in?
-    !current_user.nil?
-  end
 end
